f3918c8d52e92399e0679504f5532aea2fbc8137
Michael Callahan <mcallahan0@yahoo.com>
2021-06-25 19:26:23 -0400

    Added functionality for local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Creating an array for the standard business hours in military time format
-var times = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+var times = ["9am", "10am", "11am", "12pm", "13pm", "14pm", "15pm", "16pm", "17pm"];
 
 // Creating a variable to store the current moment
 var currentDay = moment();
@@ -9,7 +9,6 @@ $("#currentDay").text(currentDay.format("dddd, MMMM Do"));
 
 // Creating a variable to store the current time in military time format using moment
 var currentTime = moment().format("HH");
-console.log(currentTime);
 
 // A loop to create each time block on the scheduler using the times array
 for (i = 0; i < times.length; i++) {
@@ -23,25 +22,30 @@ for (i = 0; i < times.length; i++) {
     timeBlock.addClass("col-1 hour");
     
     // Checks the array index to convert the times from military time to regular time
-    if (times[i] > 12) {
-        timeBlock.text(times[i] - 12 + "pm");
-    } else if (times[i] < 12) {
-        timeBlock.text(times[i] + "am");
+    if (parseInt(times[i]) > 12) {
+        timeBlock.text(parseInt(times[i]) - 12 + "pm");
+    } else if (parseInt(times[i]) < 12 + "am") {
+        timeBlock.text(parseInt(times[i]));
     } else {
-        timeBlock.text(times[i] + "pm");
+        timeBlock.text(times[i]);
     }
 
     row.append(timeBlock);
 
     // Creates a text area to plan important events for that specific time block
+    // Creates a variable to hold the value of the text in local storage for that particular time
     // The col-10 class makes it so the text area takes up 10 columns worth of space
+    // Sets the text inside the specific text area to the plannerText local storage value for that particular time
     var planner = $('<textarea>');
-    planner.addClass("col-10");
+    var plannerText = localStorage.getItem(times[i]);
+    planner.addClass("col-10 description");
+    planner.val(plannerText);
     row.append(planner);
 
-    // Creates a button to save your planner for that specific time block to local storage
+    // Creates a button to save your planner for that specific time block to local storage and adds the font awesome save button icon to the button
     var save = $('<button>');
     save.addClass("col-1 saveBtn");
+    $(save).append('<i class="fas fa-save"/>')
     row.append(save);
 
     // Appends the row div to the div with the container class
@@ -58,3 +62,12 @@ for (i = 0; i < times.length; i++) {
 
 }
 
+// Saves the input of text area as well as the time of that input to local storage when the user clicks the save button on a specific time block
+$(".saveBtn").on("click", function () {
+
+    var index = $(this).siblings(".hour").text();
+    var input = $(this).siblings(".description").val();
+
+    localStorage.setItem(index, input);
+
+});
